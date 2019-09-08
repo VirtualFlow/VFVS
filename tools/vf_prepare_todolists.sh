c3ba957e0b3b311882725a52b8a4a0efc3107e66
cgorgulla <c.gorgulla@gmail.com>
2019-09-08 09:30:36 -0400

    Bugfix vf_prepare_todolists.sh

--- tools/vf_prepare_todolists.sh
+++ tools/vf_prepare_todolists.sh
@@ -18,7 +18,7 @@
 # along with VirtualFlow.  If not, see <https://www.gnu.org/licenses/>.
 
 #Checking the input arguments
-usage="Usage: vf_start_jobline.sh <start-jobline-id> <end-jobline-id>
+usage="Usage: vf_prepare_todolists.sh <start-jobline-id> <end-jobline-id>
 #
 #Description: Prepares the todo lists of the joblines in advance of the workflow. Uses the corresponding control files in ../workflow/control for each jobline.
 #
@@ -30,7 +30,7 @@ if [ "${1}" == "-h" ]; then
    echo -e "\n${usage}\n\n"
    exit 0
 fi
-if [ "$#" -ne "3" ]; then
+if [ "$#" -ne "2" ]; then
    echo -e "\nWrong number of arguments. Exiting..."
    echo -e "\n${usage}\n\n"
    exit 1
@@ -43,7 +43,7 @@ echo
 echo
 echo
 
-# Standard error response 
+# Standard error response
 error_response_nonstd() {
     echo "Error was trapped which is a nonstandard error."
     echo "Error in bash script $(basename ${BASH_SOURCE[0]})"
@@ -55,6 +55,7 @@ trap 'error_response_nonstd $LINENO' ERR
 determine_controlfile() {
 
     # Determining the VF_CONTROLFILE to use for this jobline
+    cd ..
     VF_CONTROLFILE=""
     for file in $(ls ../workflow/control/*-* 2>/dev/null || true); do
         file_basename=$(basename $file)
@@ -71,11 +72,7 @@ determine_controlfile() {
     if [ -z "${VF_CONTROLFILE}" ]; then
         if [[ -f ../workflow/control/all.ctrl ]]; then
 
-            if [[ "{VF_CONTROLFILE_OLD}" != "../workflow/control/all.ctrl" ]]; then
-
-                # Variables
-                export VF_CONTROLFILE="../workflow/control/all.ctrl"
-            fi
+            export VF_CONTROLFILE="../workflow/control/all.ctrl"
 
         else
             # Error response
@@ -83,6 +80,7 @@ determine_controlfile() {
             false
         fi
     fi
+    cd helpers
 }
 
 
@@ -92,17 +90,17 @@ end_jobline_no=$2
 
 # Body
 cd helpers
-for i in {$start_jobline_no..$end_jobline_no}; do
+for i in $(seq $start_jobline_no $end_jobline_no); do
 
     # Determining the controlfile
     export VF_JOBLINE_NO=$i
     determine_controlfile
 
     # Variables
-    export VF_TMPDIR="$(grep -m 1 "^tempdir=" ${VF_CONTROLFILE} | tr -d '[[:space:]]' | awk -F '[=#]' '{print $2}')"
-    export VF_JOBLETTER="$(grep -m 1 "^job_letter=" ${VF_CONTROLFILE} | tr -d '[[:space:]]' | awk -F '[=#]' '{print $2}')"
-    steps_per_job="$(grep -m 1 "^steps_per_job=" ${VF_CONTROLFILE} | tr -d '[[:space:]]' | awk -F '[=#]' '{print $2}')"
-    queues_per_step="$(grep -m 1 "^queues_per_step=" ${VF_CONTROLFILE} | tr -d '[[:space:]]' | awk -F '[=#]' '{print $2}')"
+    export VF_TMPDIR="$(grep -m 1 "^tempdir=" ../${VF_CONTROLFILE} | tr -d '[[:space:]]' | awk -F '[=#]' '{print $2}')"
+    export VF_JOBLETTER="$(grep -m 1 "^job_letter=" ../${VF_CONTROLFILE} | tr -d '[[:space:]]' | awk -F '[=#]' '{print $2}')"
+    steps_per_job="$(grep -m 1 "^steps_per_job=" ../${VF_CONTROLFILE} | tr -d '[[:space:]]' | awk -F '[=#]' '{print $2}')"
+    queues_per_step="$(grep -m 1 "^queues_per_step=" ../${VF_CONTROLFILE} | tr -d '[[:space:]]' | awk -F '[=#]' '{print $2}')"
 
     # Preparing the todolists
     bash prepare-todolists.sh $i $steps_per_job $queues_per_step;
