abdceaa712b8aff419f0c03f17cdecda4ef4e66a
Matt Koop <mkoop@amazon.com>
2023-03-24 08:54:46 -0500

    typo fix

--- cfn/create-vf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-aws cloudformation create-stack --stack-name vf \
---template-body file://vf.yaml \
---capabilities CAPABILITY_NAMED_IAM \
---parameters file://vf-parameters.json \
---region us-east-1
-
--- cfn/delete-vf.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-aws cloudformation delete-stack --stack-name vf \
---region us-east-1
-
