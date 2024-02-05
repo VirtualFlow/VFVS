e12c728dc14332732bae338d5dec0d9c531ffb4c
Akshat Nigam <akshat998@gmail.com>
2024-02-04 23:20:17 -0800

    bug resolve, remove utils file.

--- tools/templates/vfvs_run.py
+++ tools/templates/vfvs_run.py
@@ -55,14 +55,51 @@ import math
 import sys
 import uuid
 
-from .utils import load_config, format_ligand
+from typing import Dict, Tuple
 
 logger = logging.getLogger(__name__)
 
-# Download
-# -
-# Get the file and move it to a local location
-#
+def read_config_line(line: str) -> Tuple[str, str]:
+    key, sep, value = line.strip().partition("=")
+    return key.strip(), value.strip()
+
+def load_config(config_path: str) -> Dict:
+    with open(config_path) as fd:
+        result = dict(read_config_line(line) for line in fd)
+
+    for item in result:
+        if '#' in result[item]:
+            result[item] = result[item].split('#')[0]
+
+    return result
+
+def format_ligand(ligand_path: str, file_format: str) -> str:
+    """Converts a ligand file to a different file format using the Open Babel tool.
+
+        Args:
+            ligand_ (str): The path to the input ligand file.
+            new_format (str): The desired output format for the ligand file.
+    
+        Returns:
+            None
+    
+        Raises:
+            Exception: If the input file does not exist, or if the Open Babel tool is not installed.
+    
+        Examples:
+            To convert a ligand file from mol2 format to pdbqt format:
+            >>> convert_ligand_format('./ligands/ligand1.mol2', 'pdbqt')
+    """
+    ligand_path_as_list = ligand_path.split('.')
+    current_format = ligand_path_as_list[-1]
+    if current_format != file_format: 
+        logger.info(f'Converting ligand file format to {file_format} using obabel.')
+        ligand_path_as_list[-1] = file_format
+        result = ''.join(ligand_path_as_list)
+        os.system('obabel {} -O {}'.format(ligand_path, result))
+        return result
+        
+    return ligand_path
 
 def downloader(download_queue, unpack_queue, summary_queue, tmp_dir):
 
@@ -567,8 +604,8 @@ def docking_process_batch(summary_queue, scenario, items, temp_dir):
         docking_process_clean_common(batched_item)
 
     else:
-        logging.error(f"Invalid ligand processing model for program {task['program']}")
-        raise RuntimeError(f"Invalid ligand processing model for program {task['program']}")
+        logging.error(f"Invalid ligand processing model for program {scenario['program']}")
+        raise RuntimeError(f"Invalid ligand processing model for program {scenario['program']}")
 
 
     while summary_queue.qsize() > 200:
